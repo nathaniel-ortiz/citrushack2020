947c374719f3564c7930d5337210d31745710ce8
Nathaniel Ortiz <thelectronicnub@gmail.com>
2020-04-26 00:09:26 -0700

    JSON serialization completed!!!

--- get_moon.py
+++ get_moon.py
@@ -19,6 +19,5 @@ m13_GCRS = m13.transform_to(GCRS(obstime=time))
 moon_GCRS = get_moon(time, mypos)
 moon_AltAz = moon_GCRS.transform_to(AltAz(obstime=time, location=mypos))
 
-json_output = {'type': 'AltAz', 'obs_date': moon_AltAz.obstime.to_value('iso', subfmt='date'), 'obs_time': '07:33:16.792604', 'obs_geoloc': {'x': '-4555905.98999084', 'y': '-2686284.03386282', 'z': '3553520.56909795'}, 'alt_az': {'az': 'ignore', 'alt': '-6.56606571'}}
-print(json.dumps(json_output))
-print(moon_AltAz.obstime.to_value('iso', subfmt='date'))
+json_output = {'type': "AltAz", 'obs_date': moon_AltAz.obstime.to_value('iso', subfmt='date'), 'obs_time': str(moon_AltAz.obstime.ymdhms.hour) + ":" + str(moon_AltAz.obstime.ymdhms.minute) + ":" + str(moon_AltAz.obstime.ymdhms.second), 'obs_geoloc': {'x': str(moon_AltAz.location.x), 'y': str(moon_AltAz.location.y), 'z': str(moon_AltAz.location.z)}, 'alt_az': {'az': str(moon_AltAz.az), 'alt': str(moon_AltAz.alt)}}
+print(json.dumps(json_output, indent=4))
